6af974d320976857cf7d656a7ada5b9222185111
ntua-19211 <mariakoilalou@gmail.com>
2023-05-13 19:31:06 +0200

    cbc

--- RSA.py
+++ RSA.py
@@ -1,4 +1,5 @@
 import math
+import os
 import random
 import sympy
 
@@ -82,9 +83,41 @@ def decrypt_ecb(private_key, ciphertext):
     return plaintext.decode('utf-8')
 
 
+def encrypt_cbc(public_key, msg):
+    block_size = (public_key[0].bit_length() + 7) // 8
+    iv = os.urandom(block_size)
+    padded_msg = pad_message(msg.encode(), block_size)
+    blocks = [padded_msg[i:i + block_size] for i in range(0, len(padded_msg), block_size)]
+    cipher_blocks = [iv]
+    for block in blocks:
+        m = int.from_bytes(block, byteorder='big')
+        c = pow(m ^ int.from_bytes(cipher_blocks[-1], byteorder='big'), public_key[1], public_key[0])
+        cipher_blocks.append(c.to_bytes(block_size, byteorder='big'))
+    ciphertext = b''.join(cipher_blocks)
+    return ciphertext
+
+
+def decrypt_cbc(private_key, ciphertext):
+    block_size = (private_key[0].bit_length() + 7) // 8
+    iv = ciphertext[:block_size]
+    blocks = [ciphertext[i:i + block_size] for i in range(block_size, len(ciphertext), block_size)]
+    plaintext_blocks = []
+    for i, block in enumerate(blocks):
+        c = int.from_bytes(block, byteorder='big')
+        m = pow(c, private_key[1], private_key[0]) ^ int.from_bytes(iv if i == 0 else blocks[i - 1], byteorder='big')
+        plaintext_blocks.append(m.to_bytes(block_size, byteorder='big'))
+    return unpad_message(b''.join(plaintext_blocks)).decode('utf-8')
+
+
 public_key, private_key = generate_keys()
 msg = input("Enter the message you want to send: ")
-c = encrypt_ecb(public_key, msg)
-print("Ciphertext: ", c)
-dm = decrypt_ecb(private_key, c)
-print("Decrypted message:", dm)
+print("ECB: ")
+c1 = encrypt_ecb(public_key, msg)
+print("Ciphertext: ", c1)
+dm1 = decrypt_ecb(private_key, c1)
+print("Decrypted message:", dm1)
+print("CBC: ")
+c2 = encrypt_cbc(public_key, msg)
+print("Ciphertext: ", c2)
+dm2 = decrypt_cbc(private_key, c2)
+print("Decrypted message:", dm2)
